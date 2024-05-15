24524e1607df262b90488c0906f6cb8416a5c26c
Juliiaks <juliakharetonchuk@gmail.com>
2024-05-16 02:48:06 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Description from './components/Description/description'
+import Feedback from './components/Feedback/feedback'
+import Options from './components/Options/options'
 import './App.css'
+import { useState } from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const updateFeedback = feedbackType => {
+    feedbackType = ['good', 'neutral', 'bad'].map((el) => el)
+    const [click, setClick] = useState(0)
+    const handleClick = () => {
+        setClick(click + 1)
+    }
+ }
+ 
+  const feedback = {
+  good: 0,
+	neutral: 0,
+	bad: 0
+  }  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Options  onClick={updateFeedback} />
+      <Feedback
+        good={updateFeedback}
+        neutral={feedback.neutral}
+        bad={feedback.bad}
+      />
     </>
   )
 }
---
+++ src/components/Options/options.jsx
@@ -0,0 +1,18 @@
+
+//   import { useState } from 'react';
+
+export default function Options() {
+    
+// const [click, setClick] = useState(0)
+  
+//     const handleClick = () => {
+//         setClick(click + 1)
+//     }
+    return(
+    <>
+        <button >Good</button>
+        <button >Neutral</button>
+        <button >Bad</button>
+        </>
+    )
+}
